4516b7a94a6527e62feedaa3eadc31e93a6c4c09
kryzalvarez <153542377+kryzalvarez@users.noreply.github.com>
2025-08-28 12:14:47 -0600

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   
-  // --- LÍNEAS AÑADIDAS ---
+  // --- LÍNEAS AÑADIDAS ---ggg
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
