9d3d7724bda0cc1e090cf8ff315b9b67ef4504d7
kryzalvarez <sledenbeats@gmail.com>
2025-09-01 11:24:31 -0600

    Feat:  solucion erroressssssssss

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -29,7 +29,7 @@ export default function DashboardPage() {
     case 'admin':
       return <AdminDashboard />;
     case 'sales':
-      // <-- 2. Renderizamos el nuevo componente de Ventasss
+      // <-- 2. Renderizamos el nuevo componente de Ventasssss
       return <SalesDashboard />;
     default:
       return <div>Rol desconocido o no tienes acceso.</div>;
